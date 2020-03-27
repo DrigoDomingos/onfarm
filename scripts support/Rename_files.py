32d83f9adee4d049db6bea8d0ba7b293f8ab4f69
Rodrigo <domingos.drigo@gmail.com>
2020-03-26 23:57:22 -0500

    Update Rename_files.py

--- scripts support/Rename_files.py
+++ scripts support/Rename_files.py
@@ -1,7 +1,7 @@
 import os
 
-path = 'C:\Users\Rodrigo\Desktop\Azul_Claro Etapa 1 Revisado'
-i = 0
+path = "C:\\Users\\Rodrigo\\Desktop\\fotos - isolados\\azul_claro"
+i = 50
 for filename in os.listdir(path):
-    os.rename(os.path.join(path,filename), os.path.join(path,str(i)+'.jpg'))
+    os.rename(os.path.join(path,filename), os.path.join(path,'azul_claro_isoladas_'+str(i)+'.jpg'))
     i = i +1
